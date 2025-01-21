346a48796b52457b23ae1dbe3925172e5eb40752
ZakirAliyev <aliyevzakir814@gmail.com>
2025-01-21 19:48:42 +0400

    QAVO

--- src/components/BurgerMenu/index.jsx
+++ src/components/BurgerMenu/index.jsx
@@ -26,28 +26,28 @@ export default function BurgerMenu() {
                 <div className={"greenLine"}></div>
             </div>
             <div className={"links"}>
-                <Link to={`/`} className={"link"}>Giriş</Link>
-                <Link to={`/`} className={"link"}>Tanıtım</Link>
-                <Link to={`/`} className={"link"}>Portfolio</Link>
-                <Link to={`/`} className={"link"}>Partnyorlar</Link>
-                <Link to={`/`} className={"link"}>Əlaqə</Link>
+                <a href="#" className={"link"}>Giriş</a>
+                <a href="#banner" className={"link"}>Tanıtım</a>
+                <a href="#portfolio" className={"link"}>Portfolio</a>
+                <a href="#services" className={"link"}>Partnyorlar</a>
+                <a href="#contact" className={"link"}>Əlaqə</a>
             </div>
-            <div className={"lineWrapper"}>
-                <div className={"greenLine"}></div>
+            <div className={" lineWrapper"}>
+                <div className={" greenLine"}></div>
             </div>
-            <div className={"social"}>
-                <Link to={`/`} className={"icon"}><FaInstagram/></Link>
-                <Link to={`/`} className={"icon"}><FaFacebook/></Link>
-                <Link to={`/`} className={"icon"}><FaTwitter/></Link>
-                <Link to={`/`} className={"icon"}><FaTelegram/></Link>
-                <Link to={`/`} className={"icon"}><FaYoutube/></Link>
+            <div className={" social"}>
+                <Link to={`/`} className={" icon"}><FaInstagram/></Link>
+                <Link to={`/`} className={" icon"}><FaFacebook/></Link>
+                <Link to={`/`} className={" icon"}><FaTwitter/></Link>
+                <Link to={`/`} className={" icon"}><FaTelegram/></Link>
+                <Link to={`/`} className={" icon"}><FaYoutube/></Link>
             </div>
         </section>
     );
 
     return (
-        <div className={"burgerMenu"}>
-            <Button className={"gizli"} onClick={toggleDrawer(true)}><FaBars className={"menuIcon"}/></Button>
+        <div className={" burgerMenu"}>
+            <Button className={" gizli"} onClick={toggleDrawer(true)}><FaBars className={" menuIcon"}/></Button>
             <Drawer headerStyle={{
                 backgroundColor: 'black',
                 color: 'white',
@@ -55,7 +55,7 @@ export default function BurgerMenu() {
                     bodyStyle={{
                         backgroundColor: 'black',
                         padding: '20px',
-                    }} open={open} onClose={toggleDrawer(false)} anchor={"right"}
+                    }} open={open} onClose={toggleDrawer(false)} anchor={" right"}
                     sx={{
                         '& .MuiDrawer-paper': {
                             backgroundColor: '#111',
--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,13 +1,31 @@
 import './index.scss';
 
+// Resimleri import edin
+import img1 from '/src/assets/1.png';
+import img2 from '/src/assets/2.png';
+import img3 from '/src/assets/3.png';
+import img4 from '/src/assets/4.png';
+import img5 from '/src/assets/5.png';
+import img6 from '/src/assets/6.png';
+import img7 from '/src/assets/7.png';
+import img8 from '/src/assets/8.png';
+import img9 from '/src/assets/9.png';
+import img10 from '/src/assets/10.png';
+import img11 from '/src/assets/11.png';
+import img12 from '/src/assets/12.png';
+import img13 from '/src/assets/13.png';
+import img14 from '/src/assets/14.png';
+import img15 from '/src/assets/15.png';
+import img16 from '/src/assets/16.png';
+import img17 from '/src/assets/17.png';
+
+// Resimleri bir diziye ekleyin
 const imageUrls = [
-    '1.png', '2.png', '3.png', '4.png', '5.png',
-    '6.png', '7.png', '8.png', '9.png', '10.png', '11.png',
-    '12.png', '13.png', '14.png', '15.png', '16.png', '17.png'
+    img1, img2, img3, img4, img5, img6, img7, img8, img9,
+    img10, img11, img12, img13, img14, img15, img16, img17
 ];
 
 function Services() {
-
     return (
         <div className="col-12">
             <section id="services">
@@ -17,7 +35,7 @@ function Services() {
                     {imageUrls.map((url, index) => (
                         <div className="col-2 col-md-2 col-sm-6 col-xs-6" key={index}>
                             <div className="box">
-                                <img src={"/src/assets/" + url} alt={`Image ${index + 1}`}/>
+                                <img src={url} alt={`Image ${index + 1}`} />
                             </div>
                         </div>
                     ))}
